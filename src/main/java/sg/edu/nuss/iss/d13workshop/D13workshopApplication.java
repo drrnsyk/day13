eeaf0abb7ed4e1950ea6feaaae3fb2c6451164b9
drrnsyk <darren.syk@gmail.com>
2022-07-20 09:54:39 +0800

    initial commit

---
+++ src/main/java/sg/edu/nuss/iss/d13workshop/D13workshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nuss.iss.d13workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D13workshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D13workshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nuss/iss/d13workshop/D13workshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nuss.iss.d13workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D13workshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
